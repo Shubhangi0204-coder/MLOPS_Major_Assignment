d1eb652a85817f5c4b14ea3bf8c676b840f0d366
Shubhangi <shubhangi.katyani@gmail.com>
2025-11-19 09:26:29 +0530

    commit

--- train.py
+++ train.py
@@ -2,7 +2,7 @@
 from sklearn.datasets import fetch_olivetti_faces
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
-import joblib
+import joblib, pickle
 import os
 
 def main():
